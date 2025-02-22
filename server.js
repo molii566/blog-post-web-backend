e534a346e3cece7e34d6a98f0b52a559dfb5b3af
Mo Lii <molii566@users.noreply.github.com>
2025-02-22 17:57:30 +0300

    added users login

--- server.js
+++ server.js
@@ -25,13 +25,23 @@ app.use(express.static("public"));
 
 app.use(function (req, res, next) {
     res.locals.errors = []; // Corrected spelling to `res.locals.errors`
+
+    // decoding the incoming cookie
+    try {
+        const decoded = jwt.verify(req.cookies.mysimpleapp, process.env.JWTSECRET);
+        req.user = decoded;
+    } catch (err) {
+        req.user = false;
+    }
+    res.locals.user = req.user;
+    console.log(req.user);
     next();
 });
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    res.render("homepage");
+    res.render("homepage", { user: res.locals.user, errors: res.locals.errors });
 });
 
 app.get("/login", (req, res) => {
@@ -84,7 +94,41 @@ app.post("/register", (req, res) => {
 
     
 });
+app.post("/login", (req, res) => {
 
+    const errors = [];
+    if (typeof req.body.username !== "string") req.body.username = "";
+    if (typeof req.body.password !== "string") req.body.password = "";
+
+    req.body.username = req.body.username.trim();
+    if (!req.body.username) errors.push("You must enter a username");
+    if (req.body.username && req.body.username.length < 3) errors.push("Username must be more than 3 characters");
+    if (req.body.username && req.body.username.length > 10) errors.push("Username must be 10 or fewer characters");
+    if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Username must contain only numbers and letters"); 
+    if (errors.length) {
+        return res.render("login", { errors: errors });
+    }
+    const lookupstmt = db.prepare("SELECT * FROM users WHERE username = ?");
+    const ouruser = lookupstmt.get(req.body.username);
+    if (!ouruser) {
+        return res.render("login", { errors: ["invalid Username / password"] });
+    }
+
+    const matchornot = bcrypt.compareSync(req.body.password, ouruser.password);
+    if (!matchornot) {
+        return res.render("login", { errors: ["invalid Username / password"] });
+    }
+    // give the user a cookie
+    const ourtoken = jwt.sign({exp:Math.floor(Date.now() / 1000) + 60 *60 * 24 ,skycolor:"blue" , userid: ouruser.id ,username:ouruser.username} ,process.env.JWTSECRET);
+    res.cookie("mysimpleapp",ourtoken,{
+    httpOnly: true ,
+    secure: true ,
+    sameSite:"strict",
+    maxAge: 1000 * 60 * 60 * 24
+    
+    })
+    res.redirect("/");
+})
 const port = 8000;
 app.listen(port, () => {
     console.log(`I'm on port ${port}`);
